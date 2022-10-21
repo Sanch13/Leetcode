f4aa9e1b92844f048596cb217dcffb2b15f143e6
a.zubchyk <Sanch13sanch@gmail.com>
2022-10-21 17:01:03 +0300

    tasks

---
+++ Tasks_1.py
@@ -0,0 +1,28 @@
+import string
+
+"""1. Two Sum
+Given an array of integers nums and an integer target, return indices of the two numbers such 
+that they add up to target. You may assume that each input would have exactly one solution, 
+and you may not use the same element twice. You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1]."""
+
+
+class Solution:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if target == nums[i] + nums[j]:
+                    return [i, j]
+
+
+obj = Solution()
+# print(obj.twoSum([2, 7, 11, 15], target=9))     # [0, 1]
+# print(obj.twoSum([3, 2, 4], target=6))          # [1, 2]
+# print(obj.twoSum([3, 1, 4, 2], target=6))       # [2, 3]
+# print(obj.twoSum([3, 2, 3], target=6))          # [0, 2]
+# print(obj.twoSum([2, 5, 5, 11], target=10))     # [1, 2]
+
---
+++ Tasks_724.py
@@ -0,0 +1,46 @@
+import sys
+
+"""Given an array of integers nums, calculate the pivot index of this array.
+The pivot index is the index where the sum of all the numbers strictly to the left of the index is 
+equal to the sum of all the numbers strictly to the index's right.
+If the index is on the left edge of the array, then the left sum is 0 because there are no 
+elements to the left. This also applies to the right edge of the array.
+Return the leftmost pivot index. If no such index exists, return -1.
+
+Example 1:
+
+Input: nums = [1,7,3,6,5,6]
+Output: 3
+Explanation:
+The pivot index is 3.
+Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+Right sum = nums[4] + nums[5] = 5 + 6 = 11"""
+
+
+class Solution:
+    def pivotIndex(self, nums: list[int]) -> int:
+        S = sum(nums)
+        left_sum = 0
+        for i, x in enumerate(nums):
+            if left_sum == (S - left_sum - x):
+                return i
+            left_sum += x
+        return -1
+
+
+obj = Solution()
+# print(obj.pivotIndex([2, 1, 3, 2, 1]))  # 2
+# print(obj.pivotIndex([1, 7, 3, 6, 5, 6]))   # 3
+# print(obj.pivotIndex([1, 2, 3]))   # -1
+# print(obj.pivotIndex([2, 1, -1]))   # 0
+# print(obj.pivotIndex([-1, -1, -1, -1, -1, 0]))   # 2
+
+
+
+
+
+
+
+
+
+
