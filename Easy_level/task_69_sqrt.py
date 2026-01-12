f28a9636d7707f5c840cdef7fcbf49751a0a98ce
Aliaksandr Zubchyk <sanch13sanch@gmail.com>
2026-01-12 19:12:59 +0300

    task 69

---
+++ Easy_level/task_69_sqrt.py
@@ -0,0 +1,50 @@
+"""
+69. Sqrt(x)
+Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
+The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+Constraints:
+0 <= x <= 231 - 1
+"""
+class Solution:
+	def mySqrt(self, x: int) -> int:
+		if x == 1 or x == 0:
+			return x
+
+		left = 0
+		right = x
+
+		while left <= right:
+			mid = (left + right) // 2
+
+			sqr = mid * mid
+
+			if  sqr == x:
+				return mid
+			elif sqr < x:
+				left = mid + 1
+			else:
+				right = mid - 1
+
+		return right
+
+
+if __name__ == '__main__':
+	x = 8
+	s = Solution()
+	print(s.mySqrt(x))
